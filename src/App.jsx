35812a4c4dd1262bfdf7b573e5ef8a9183dc0ba7
Elton Benjamin <eltobenjo@gmail.com>
2023-06-22 16:28:12 +0200

    Pictures

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SideNav from './components/SideNav'
+
+import './App.css'
+import Main from './components/Main'
+
+function App() {
+ 
+
+  return (
+    <>
+    <SideNav/>
+    <Main/>
+    
+    </>
+  )
+}
+
+export default App
